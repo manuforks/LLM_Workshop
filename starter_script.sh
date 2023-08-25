f4bc769fc8fc7a46042f399b16a81f2d8e295df0
Deepak Sharma <DeepakGitH@users.noreply.github.com>
2023-08-26 02:42:16 +0530

    Update starter_script.sh

--- starter_script.sh
+++ starter_script.sh
@@ -8,6 +8,8 @@ sudo apt install git-lfs
 python3 -m venv llmenv
 #Python Package Install
 llmenv/bin/pip install jupyter
+llmenv/bin/pip install scipy
+llmenv/bin/pip install scikit-learn
 llmenv/bin/pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
 llmenv/bin/pip install git+https://github.com/huggingface/transformers
 llmenv/bin/pip install -q accelerate==0.21.0 peft==0.4.0 bitsandbytes==0.41.1 trl==0.4.7
