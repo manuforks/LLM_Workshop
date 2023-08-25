088937e31a48cb1f8b0452b8d1feda400c66f704
Deepak Sharma <DeepakGitH@users.noreply.github.com>
2023-08-25 17:35:16 +0530

    Update starter_script.sh

--- starter_script.sh
+++ starter_script.sh
@@ -10,7 +10,7 @@ python3 -m venv llmenv
 llmenv/bin/pip install jupyter
 llmenv/bin/pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
 llmenv/bin/pip install git+https://github.com/huggingface/transformers
-llmenv/bin/pip install -q accelerate==0.21.0 peft==0.4.0 bitsandbytes==0.40.2 trl==0.4.7
+llmenv/bin/pip install -q accelerate==0.21.0 peft==0.4.0 bitsandbytes==0.41.1 trl==0.4.7
 llmenv/bin/pip install -q -U datasets
 llmenv/bin/pip install -q -U einops
 llmenv/bin/pip install -q -U wandb
